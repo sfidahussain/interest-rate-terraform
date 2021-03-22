52ae0532e20a810302cf54241d7775b405fbb54c
sfidahussain <6472383+sfidahussain@users.noreply.github.com>
2021-03-22 18:30:33 -0500

    replacing with dynamodb
    replacing with dynamodb

--- main.tf
+++ main.tf
@@ -10,19 +10,5 @@ terraform {
 # Default region specified as us-west-2
 provider "aws" {
   profile = "default"
-  region  = "us-west-2"
-}
-
-# DB Postgres Engine
-# This is where data is persisted when a user uploads a file
-# with file information and interest rate records within each file.
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
-  engine               = "postgres"
-  engine_version       = "11.10"
-  instance_class       = "db.t3.micro"
-  name                 = "devdb"
-  username             = "foo"
-  password             = "foobarbaz"
-  skip_final_snapshot  = true
+  region  = "us-east-1"
 }
