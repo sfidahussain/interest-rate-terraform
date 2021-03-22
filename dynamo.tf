2ff2335104173b4d4f117a2d82e7632c1d01ada4
sfidahussain <6472383+sfidahussain@users.noreply.github.com>
2021-03-22 18:32:09 -0500

    adding comments
    adding comments

--- dynamo.tf
+++ dynamo.tf
@@ -1,3 +1,9 @@
+# Provisions a DynamoDB Table containing Rate Type, Rate Value, and Timestamp
+# Lambda functions read from this table to get the latest rate,
+# a specific rate, and All Rates.
+# The primary key is its timestamp and that's the only attribute defined.
+# The others are not needed to be defined. They are defined once a user uploads
+# a file.
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
   name           = "Rates"
   billing_mode   = "PROVISIONED"
